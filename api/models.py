6b48dfd352fd6c77ecc4890fe241dd967c21be55
Onuchi Johnson <Onuchi.J@bullseye.local>
2023-06-24 08:39:34 +0100

    router

--- api/models.py
+++ api/models.py
@@ -5,22 +5,26 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
+    
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
 
-    def _str_(self):
+
+    def __str__(self):
         return self.title
 
 
@@ -30,34 +34,5 @@ class Ingredient(models.Model):
     quantity = models.IntegerField()
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
-
-
-class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Ingredient.objects.all())
-    tags = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Tag.objects.all())
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'description', 'ingredients', 'tags']
-
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = ['id', 'name', 'description', 'quantity']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
